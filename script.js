9cdc945cbed14125085de5ef1606bf2cec22e43a
曾子恩 <tsengzien@zengziendeMacBook-Air.local>
2022-08-22 16:09:51 +0800

    Feat:新增提示功能

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 
 
+var random;
 
-
-
-$('button').click(function(){
-    $.getJSON('./EoMword.json', function(data){
-        var random =  Math.floor(Math.random() * 44999)
+$('.word').click(function(){
+    $('h2').text('')
+    $.getJSON('./thing.json', function(data){
+        getRandom()
         var answer = data.data[random].title
         $('h1').text(answer)
     });
     
 });
+
+$('.hint').click(function(){
+    $.getJSON('./thing.json', function(data){
+        var hint = data.data[random].note
+        $('h2').text(hint)
+    })
+});
+
+function getRandom(){
+    random =  Math.floor(Math.random() * 44999)
+}
