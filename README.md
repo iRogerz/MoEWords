649d8910008a932dd135876a2092f0cb75c90768
Roger Tseng <rogertseng1025@gmail.com>
2022-12-26 22:22:28 +0800

    feat(mod): 把一個字的從自詞庫刪除

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 隨機詞語生產器
-Hello, 我是Roger,as IOS Developer
-這個專案是用來隨機生出詞語的系統，使用的是教育部簡編本字詞。第一次點擊時要載入所以會延遲許久，之後再點擊就沒問題囉
+
+## [網站連結](https://irogerz.github.io/MoEWords/)
+
+Hello, 我是 Roger,as IOS Developer
+這個專案是用來隨機生出詞語的系統，使用的是教育部簡編本字詞。第一次點擊時要載入所以會延遲，之後再點擊就沒問題囉
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var random;
 let t;
 let myTimer;
@@ -10,93 +8,88 @@ let is_startBtn_clicked = false;
 
 reset();
 
-$('.word').click(function(){
-    if(is_startBtn_clicked && !is_time_up){
-        newWord();
-    }
+$(".word").click(function () {
+  if (is_startBtn_clicked && !is_time_up) {
+    newWord();
+  }
 });
 
-$('.hint').click(function(){
-    $.getJSON('./thing.json', function(data){
-        var hint = data.data[random].note
-        $('#hint').text(hint)
-    })
+$(".hint").click(function () {
+  $.getJSON("./EoMword.json", function (data) {
+    var hint = data.data[random].note;
+    $("#hint").text(hint);
+  });
 });
 
-function getRandom(){
-    random =  Math.floor(Math.random() * 44999)
+function getRandom() {
+  random = Math.floor(Math.random() * 44999);
 }
 
 //產生新詞
-function newWord(){
-    $('#hint').text('')
-    $.getJSON('./thing.json', function(data){
-        getRandom()
-        var answer = data.data[random].title
-        $('#word').text(answer)
-    });
+function newWord() {
+  $("#hint").text("");
+  $.getJSON("./EoMword.json", function (data) {
+    getRandom();
+    var answer = data.data[random].title;
+    $("#word").text(answer);
+  });
 }
 
+$("#startBtn").click(start);
+$("#reset").click(reset);
+$("#correctBtn").click(addOnePoint);
 
-
-$('#startBtn').click(start);
-$('#reset').click(reset);
-$('#correctBtn').click(addOnePoint);
-
-function start(){
-    inputTime = $('#inputText').val();
-    if(inputTime <= 0){
-        $('#second').text("時間需大於 0！");
-        $('#second').show();
-    }
-    else{
-        is_startBtn_clicked = true;
-        myScore = 0;
-        $('#second').text(inputTime);
-        myTimer = setInterval(minusOneSecond, 1000);
-        $('#second').show();
-        $('#startBtn').hide();
-        $('#scoreboard').show();
-        $('#reset').show();
-        $('#word').show();
-        $('#hint').show();
-        $('#inputText').val(null);
-        $('#inputText').hide();
-    }
-}
-
-
-function minusOneSecond(){
-    if(inputTime > 0){
-        inputTime --;
-        $('#second').text(inputTime);
-    }
-    else{
-        clearInterval(myTimer);
-        second.innerText = "Time's up!";
-        is_time_up = true;
-    }
-}
-
-function addOnePoint(){
-    if(!is_time_up){
-        myScore++;
-        score.innerText = myScore;
-    }
-}
-
-function reset(){
-    $('#second').hide();
-    $('#startBtn').show();
-    $('#scoreboard').hide();
-    $('#reset').hide();
-    $('#word').hide();
-    $('#hint').hide();
-    $('#inputText').show();
+function start() {
+  inputTime = $("#inputText").val();
+  if (inputTime <= 0) {
+    $("#second").text("時間需大於 0！");
+    $("#second").show();
+  } else {
+    is_startBtn_clicked = true;
     myScore = 0;
+    $("#second").text(inputTime);
+    myTimer = setInterval(minusOneSecond, 1000);
+    $("#second").show();
+    $("#startBtn").hide();
+    $("#scoreboard").show();
+    $("#reset").show();
+    $("#word").show();
+    $("#hint").show();
+    $("#inputText").val(null);
+    $("#inputText").hide();
+  }
+}
+
+function minusOneSecond() {
+  if (inputTime > 0) {
+    inputTime--;
+    $("#second").text(inputTime);
+  } else {
+    clearInterval(myTimer);
+    second.innerText = "Time's up!";
+    is_time_up = true;
+  }
+}
+
+function addOnePoint() {
+  if (!is_time_up) {
+    myScore++;
     score.innerText = myScore;
-    is_time_up = false;
-    is_startBtn_clicked = false;
-    newWord();
-    //原本是按 Start 才 newWord，但是每次顯示之後都要先跑一下才會換新的詞，所以改成reset時就先換好詞
+  }
+}
+
+function reset() {
+  $("#second").hide();
+  $("#startBtn").show();
+  $("#scoreboard").hide();
+  $("#reset").hide();
+  $("#word").hide();
+  $("#hint").hide();
+  $("#inputText").show();
+  myScore = 0;
+  score.innerText = myScore;
+  is_time_up = false;
+  is_startBtn_clicked = false;
+  newWord();
+  //原本是按 Start 才 newWord，但是每次顯示之後都要先跑一下才會換新的詞，所以改成reset時就先換好詞
 }
